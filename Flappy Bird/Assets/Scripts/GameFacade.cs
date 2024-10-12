c07d5279cd4c9c41bd369b59724b3080a7b33136
Guillem Alqueza Mayench <guillem.alqueza@estudiantat.upc.edu>
2024-10-12 10:14:47 +0200

    Game facade

---
+++ Flappy Bird/Assets/Scripts/GameFacade.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFacade : MonoBehaviour
+{
+    private void StartMenu()
+    {   
+
+    }
+
+    private void StartGame()
+    {
+
+    }
+
+    private void PauseGame()
+    {
+
+    }
+
+    private void FinishGame()
+    {
+
+    }
+}
